60f857952feb048628b2c880963f383ddc53315d
Erick González Sánche <erick.gonzalezs@outlook.com>
2022-03-01 18:53:36 -0600

    Se añaden endpoints a controllers

--- Digibox.Api/Controllers/v1/CustomerController.cs
+++ Digibox.Api/Controllers/v1/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Command;
 using Application.DTOs;
 using Application.Queries;
 using GoalIt.Core.Application.DTOs;
@@ -11,6 +12,22 @@ namespace Digibox.Api.Controllers.v1
   public class CustomerController : BaseApiController
   {
     /// <summary>
+    /// Create Customer
+    /// </summary>
+    /// <remarks>
+    ///   Evaluation point:  1.	Create a first endpoint to receive json data to store a customerId and customerName in database.
+    /// </remarks>
+    /// <returns></returns>
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [HttpPost()]
+    public async Task<Response<string>> PostDigiBoxCustomer(
+      [FromBody] CreateCustomerReqDto payload)
+    {
+      return await Mediator.Send(new CreateCustomerCommand(payload));
+    }
+    
+    /// <summary>
     /// Get Customer List
     /// </summary>
     /// <remarks>
@@ -21,11 +38,30 @@ namespace Digibox.Api.Controllers.v1
     [ApiVersion("1.0")]
     [ApiExplorerSettings(GroupName = "v1")]
     [HttpGet()]
-    public async Task<Response<List<CustomerSimpleResDto>>> GetRatingsReport(
+    public async Task<Response<List<CustomerSimpleResDto>>> GetDigiBoxCustomers(
       [FromQuery] PaginatedFilteredReqDto filter)
     {
-      string route = Request.Path.Value;
       return await Mediator.Send(new GetCustomerListQuery());
     }
+
+    /// <summary>
+    /// Get Customer List
+    /// </summary>
+    /// <remarks>
+    ///   Evaluation point:  3.	Create a third endpoint that receive the customerId as parameter and retrieve the information of the customer
+    ///                         (if exists) in xml format based on the schema of the Digibox Addenda https://aplicacion.digibox.com.mx/addenda/camposadicionales.xsd
+    ///                         Recommended to use serialization and libraries to generate classes from XSD schemas)
+    /// </remarks>
+    /// <param name="customerId"></param>
+    /// <returns></returns>
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [HttpGet("{customerId}")]
+    public async Task<Response<List<CustomerSimpleResDto>>> GetDigiBoxCustomerById(
+      [FromRoute] string customerId)
+    {
+      return await Mediator.Send(new GetCustomerListQuery());
+    }
+    
   }
 }
