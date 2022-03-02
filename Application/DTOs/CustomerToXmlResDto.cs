b4a990fac9107bbee2b23aa6c0e5d28e7c9b22a4
Erick González Sánche <erick.gonzalezs@outlook.com>
2022-03-01 18:48:53 -0600

    Se añaden Mappers

--- Application/DTOs/CustomerToXmlResDto.cs
+++ Application/DTOs/CustomerToXmlResDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Application.DTOs
 {
-  public class CustomerSimpleResDto
+  public class CustomerToXmlResDto
   {
     public string CustomerId { get; set; }
     public string CustomerName { get; set; }
---
+++ Application/Mappings/CustomerMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+
+namespace Application.Mappings
+{
+  public class CustomerMappingProfile:  AutoMapper.Profile
+  {
+    public CustomerMappingProfile()
+    {
+      CreateMap<CreateCustomerReqDto, CustomerEntity>()
+        .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.CustomerName))
+        .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.CustomerId))
+        .ReverseMap();
+      CreateMap<CustomerSimpleResDto, CustomerEntity>()
+        .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.CustomerName))
+        .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.CustomerId))
+        .ReverseMap();
+      CreateMap<CustomerToXmlResDto, CustomerEntity>()   
+        .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.CustomerName))
+        .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.CustomerId))
+        .ReverseMap();
+    }
+  }
+}
